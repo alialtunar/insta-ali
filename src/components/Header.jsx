f0a63839c38ec10846a6ca3e249a56f48c847f4d
alialtunar <altunarali284@gmail.com>
2024-06-09 16:24:40 +0300

    add next-auth and modify header

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+"use client"
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { signIn,useSession,signOut } from 'next-auth/react'
+
+const Header = () => {
+    const {data:session} = useSession();
+    console.log(session);
+  return (
+    <div className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
+      <div className="flex justify-between items-center max-w-6xl mx-auto">
+      <Link href='/' className='hidden lg:inline-flex'>
+          <Image
+            src='/Instagram_logo_black.webp'
+            width={96}
+            height={96}
+            alt='instagram logo'
+          />
+        </Link>
+
+        <Link href='/' className='lg:hidden'>
+          <Image
+            src='/800px-Instagram_logo_2016.webp'
+            width={40}
+            height={40}
+            alt='instagram logo'
+          />
+        </Link>
+
+       <input type="text" placeholder='Search' className='bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]' />
+
+       {session ? (
+         
+      <img className='h-10 w-10 rounded-full cursor-pointer' onClick={signOut} src={session.user.image} alt={session.user.name}  />
+       ):(
+        <button onClick={()=> signIn()} className='text-sm font-semibold text-blue-500'>Log In</button>
+    )}
+      </div>
+    </div>
+  )
+}
+
+export default Header
